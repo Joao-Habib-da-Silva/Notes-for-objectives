02c55d6bc817f0db219d024ac140818ceac87b33
Joao Habib da Silva <contajh544@gmail.com>
2025-02-26 17:41:32 -0300

    Check option

--- script.js
+++ script.js
@@ -4,8 +4,14 @@ function Note() {
     var notas = window.document.getElementById("afazeres")
     const new_div = window.document.createElement('div')
     new_div.classList.add("Anotações")
-    new_div.innerHTML += `<h1>${value}</h1><p> ${descrition}</p>`
+    new_div.innerHTML += `<i class='bx bx-check-square' id='correct' onclick="c()"></i><h1>${value}</h1><p> ${descrition}</p>`
+
     notas.appendChild(new_div)
+    }   
+
+function c() {
+    var icone = window.document.getElementById("correct")
+    icone.classList.toggle("green")
 }
 window.addEventListener('keydown', function(event) {
     if(event.key === "Enter") {
